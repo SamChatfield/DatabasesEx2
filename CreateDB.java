c47188a7fa397e385e845d233d3e47efff5b0182
SamChatfield <samchatfield97@gmail.com>
2017-01-10 20:38:15 +0000

    Begin CreateDB

---
+++ CreateDB.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class CreateDB {
+	
+	public CreateDB() {
+		Scanner in = new Scanner(System.in);
+		String username;
+		String password;
+		String database = "cslibrary";
+		String url = "jdbc:postgresql://mod-intro-databases.cs.bham.ac.uk/" + database;
+		
+		username = in.nextLine();
+		password = in.nextLine();
+		
+		Connection conn = connectDB(url, username, password);
+		
+		makeTables(conn);
+		populateTables(conn);
+	}
+	
+	private void makeTables(Connection conn) {
+		System.out.println("makeTables");
+	}
+	
+	private void populateTables(Connection conn) {
+		System.out.println("populateTables");
+	}
+	
+	private Connection connectDB(String url, String user, String pass) {
+		Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, username, pass);
+        } catch (SQLException ex) {
+            System.out.println("Ooops, couldn't get a connection");
+            System.out.println("Check that <username> & <password> are right");
+            System.exit(1);
+        }
+        
+        if (conn != null) {
+            System.out.println("Database accessed!");
+        } else {
+            System.out.println("Failed to make connection");
+            System.exit(1);
+        }
+        
+        return conn;
+	}
+	
+	public static void main(String[] args) {
+		try {
+            //Load the PostgreSQL JDBC driver
+            Class.forName("org.postgresql.Driver");
+
+        } catch (ClassNotFoundException ex) {
+        	ex.printStackTrace();
+            System.out.println("Driver not found");
+            System.exit(1);
+        }
+		
+		System.out.println("PostgreSQL driver registered.");
+		
+        CreateDB createDB = new CreateDB();
+	}
+	
+}
